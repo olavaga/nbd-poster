b26289fa7b3315cc9385b35a610c5c4e3c5d690e
Olav Aga <olavnlaga@gmail.com>
2024-05-27 18:29:03 +0200

    :sparkles: Add a fitted model to enable plotting without running the model and add last plot to run all

--- run_all.R
+++ run_all.R
@@ -1,5 +1,9 @@
 source("scripts/world_map.R")
 source("scripts/upset.R")
+source("scripts/run_hypertraps.R")
+source("scripts/bubbles.R")
 
 plot.world.map()
 plot.upset()
+load("fitted-model.Rdata") #model.fit <- run.hypertraps()
+plot.bubbles(model.fit)
